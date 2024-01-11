08209857b3a41bc5a185a13a0b7c97487e0db031
jamy2910 <jamiecolley02@gmail.com>
2024-01-11 03:32:36 +0000

    More CSS fixes

--- client/src/components/ProfileNavCard.jsx
+++ client/src/components/ProfileNavCard.jsx
@@ -7,7 +7,7 @@ const ProfileNavCard = ({title, description, buttonText, to}) => {
     const navigate = useNavigate();
 
   return (
-    <div className='border-2 border-solid border-emerald-700 p-4'>
+    <div className='border-2 rounded border-solid border-emerald-700 p-4'>
         <h2>{title}</h2>
         <p>{description}</p>
         <StandardButton onClick={() => {navigate(to)}}>{buttonText}</StandardButton>
